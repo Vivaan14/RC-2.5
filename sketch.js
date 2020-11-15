80636afc2ba92667a580b383d58ed0d67f143d4c
Vivaan14 <vivaanagrawal140208@gmail.com>
2020-11-15 16:40:15 +0530

    added animation and gamestate end

--- sketch.js
+++ sketch.js
@@ -10,7 +10,14 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
-
+function preload(){
+car1_img = loadImage("../images/car1.png");
+car2_img = loadImage("../images/car2.png");
+car3_img = loadImage("../images/car3.png");
+car4_img = loadImage("../images/car4.png");
+ground_img = loadImage("../images/ground.png");
+track_img = loadImage("../images/track.png");
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -27,5 +34,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState ===2){
+  game.end();
   }
 }
